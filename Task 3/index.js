5eb68d2d9e233beac276373ff2cf898b0359f561
Herdey Lord <herdeylord@Herdeys-MacBook-Pro.local>
2023-07-27 12:54:14 +0100

    add Task 3 screenshot

--- Task 3/index.js
+++ Task 3/index.js
@@ -1,5 +1,57 @@
 const mernStackDevelopers = [];
 
+const users = {
+  Alex: {
+    email: 'alex@alex.com',
+    skills: ['HTML', 'CSS', 'JavaScript'],
+    age: 20,
+    isLoggedIn: false,
+    points: 30
+  },
+  Asab: {
+    email: 'asab@asab.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'Redux', 'MongoDB', 'Express', 'React', 'Node'],
+    age: 25,
+    isLoggedIn: false,
+    points: 50
+  },
+  Brook: {
+    email: 'daniel@daniel.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
+    age: 30,
+    isLoggedIn: true,
+    points: 50
+  },
+  Daniel: {
+    email: 'daniel@alex.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'Python'],
+    age: 20,
+    isLoggedIn: false,
+    points: 40
+  },
+  John: {
+    email: 'john@john.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js'],
+    age: 20,
+    isLoggedIn: true,
+    points: 50
+  },
+  Thomas: {
+    email: 'thomas@thomas.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'React'],
+    age: 20,
+    isLoggedIn: false,
+    points: 40
+  },
+  Paul: {
+    email: 'paul@paul.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'MongoDB', 'Express', 'React', 'Node'],
+    age: 20,
+    isLoggedIn: false,
+    points: 40
+  }
+}
+
 for (const user in users) {
   const skills = users[user].skills;
   if (
@@ -12,7 +64,7 @@ for (const user in users) {
   }
 }
 
-console.log(mernStackDevelopers);
+console.log("mern stack users: ",mernStackDevelopers);
 
 const newUsers = { ...users };
 newUsers.YourName = {
@@ -23,13 +75,13 @@ newUsers.YourName = {
   points: 0
 };
 
-console.log(newUsers);
+console.log("new users: ",newUsers);
 
 const keys = Object.keys(users);
-console.log(keys);
+console.log("keys of users: ", keys);
 
 const values = Object.values(users);
-console.log(values);
+console.log("values of all key users: ", values);
 
 let maxSkills = 0;
 let skillfulPerson = '';
@@ -42,4 +94,4 @@ for (const user in users) {
   }
 }
 
-console.log(skillfulPerson);
+console.log("most skillfull person is: ",skillfulPerson);
